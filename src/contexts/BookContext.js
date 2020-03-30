5d5beb6af682625e4f9859b8faf05573ddffbc8a
amanda <amanda-jose@live.com>
2020-03-30 17:59:23 -0400

    replaced useState hook with useReducer

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,24 +1,13 @@
-import React, { createContext, useState } from "react";
-import uuid from "uuid/v1";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, setBooks] = useState([
-    { title: "Six of Crows", author: "Leigh Bardugo", id: 1 },
-    { title: "Steel Crow Saga", author: "Paul Kreuger", id: 2 }
-  ]);
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title: title, author: author, id: uuid() }]);
-  };
-
-  const removeBook = id => {
-    setBooks(books.filter(book => book.id !== id));
-  };
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {/* represents the components that the provider will wrap */}
       {props.children}
     </BookContext.Provider>
